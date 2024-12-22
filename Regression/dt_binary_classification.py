fe43a32615d18d600c2a21b8ada6b7a3166e5805
Enia Çerri <128168490+ecerri22@users.noreply.github.com>
2024-12-22 13:59:03 +0100

    add max_depth in classifier

--- Regression/dt_binary_classification.py
+++ Regression/dt_binary_classification.py
@@ -30,7 +30,7 @@ y = data["G3_binary"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train a decision tree classifier
-classifier = DecisionTreeClassifier(random_state=42)
+classifier = DecisionTreeClassifier(random_state=42, max_depth=3)
 classifier.fit(X_train, y_train)
 
 # Make predictions
